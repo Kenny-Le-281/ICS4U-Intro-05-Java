92986488c30714e84f4a4a95fe3b27bc554bb384
Kenny Le <kenny.le@mths.ca>
2024-02-14 23:33:38 +0000

    added Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,41 @@
+/*
+World program implements an application that
+World program implements an application that
+* simply displays "Hello World!" to the standard output.
+* *
+* * @author  Mr Coxall
+* * @version 1.0
+* * @since   2020-01-01
+* */
+
+/**
+ * * This is the standard "Hello, World!" program.
+ * */
+final class HelloWorld {
+
+	    /**
+	     *     * Prevent instantiation.
+	     *         * Throw an exception IllegalStateException.
+	     *             * if this is ever called
+	     *                 *
+	     *                     * @throws IllegalStateException if this is ever called
+	     *                         *
+	     *                             */
+	    private HelloWorld() {
+		            throw new IllegalStateException("Cannot be instantiated");
+			        }
+
+	        /**
+		 *     * The starting main() function.
+		 *         *
+		 *             * @param args No args will be used
+		 *                 */
+	        public static void main(final String[] args) {
+
+		        // print out "Hello, World!"
+		        System.out.println("Hello, World!");
+				
+		            System.out.println("\nDone.");
+		           }
+	                }
+			
